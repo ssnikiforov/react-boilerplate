afead3c480b512d6b0ff4307bf6bfcf6a30dfdeb
ssnikiforov <stepan001@yandex.ru>
2017-04-29 13:58:46 +0300

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+/*
+ USAGE:
+ 1. With this config file: just type `webpack` from the project-root dir
+ 2. Without this config file: `webpack ./public/app.js ./public/bundle.js`
+ 3. Run webpack in watching mode: `webpack -w`
+ */
+
+module.exports = {
+    entry: './app/app.jsx',
+    output: {
+        path: __dirname,
+        filename: './public/bundle.js'
+    },
+    resolve: {
+        root: __dirname,
+        alias: {
+
+        },
+        extensions: ['', '.js', '.jsx']
+    },
+    module: {
+        loaders: [
+            {
+                loader: 'babel-loader',
+                query: {
+                    presets: ['react', 'es2015'] //this presets are already included via package.json
+                },
+                test: /\.jsx?$/,
+                exclude: /(node_modules|bower_components)/
+            }
+        ]
+    }
+};
